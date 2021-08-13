58dd781897f978da3479590caf7c669ad7a143e6
Simon <szymon@kuffel.io>
2021-08-13 12:07:32 +0200

    Fixed list pagination

--- src/modules/paginateList.ts
+++ src/modules/paginateList.ts
@@ -3,8 +3,8 @@ import { PackageInterface } from './packageClass'
 const paginateList = (list: PackageInterface[]) => {
     var paginatedList = [] // [[Package, Package, Package, Package, Package], ...]
 
-    for (var i: number = 0; i < list.length; i += 5) {
-        paginatedList.push(list.splice(i, 5))
+    for (var i: number = 0; i < Math.ceil(list.length / 5); i++) {
+        paginatedList.push(list.slice(i * 5, i * 5 + 5))
     }
 
     return paginatedList
