7d21b6b4d0fd80b817650a7f0b9588b1545ae761
Simon <szymon@kuffel.io>
2022-01-04 19:17:57 +0100

    Fix: Release Notes

--- src/PackageBot.ts
+++ src/PackageBot.ts
@@ -101,7 +101,7 @@ client.once('ready', async () => {
 })
 
 const versions = [
-    { version: 'Version 3.2.1 (Current)', changelog: ' - Auto Detect Coureir' },
+    { version: 'Version 3.3.0 (Current)', changelog: ' - Auto Detect Coureir' },
     { version: 'Version 3.2.0', changelog: ' - Added Spam Protection' },
     { version: 'Version 3.1.1', changelog: ' - Bugfixes Regarding Reactions' },
     { version: 'Version 3.1.0', changelog: ' - English Language Support' },
