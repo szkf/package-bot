20be8a17d02c808a6042c446c413c314dc61ca5c
Simon <szymon@kuffel.io>
2022-08-29 10:05:00 +0200

    fix: update intents

--- src/PackageBot.ts
+++ src/PackageBot.ts
@@ -16,12 +16,7 @@ const client = new Discord.Client({
     intents: [
         'GUILDS',
         'GUILD_MEMBERS',
-        'GUILD_BANS',
         'GUILD_EMOJIS_AND_STICKERS',
-        'GUILD_INTEGRATIONS',
-        'GUILD_WEBHOOKS',
-        'GUILD_INVITES',
-        'GUILD_VOICE_STATES',
         'GUILD_MESSAGES',
         'GUILD_MESSAGE_REACTIONS',
         'GUILD_MESSAGE_TYPING',
